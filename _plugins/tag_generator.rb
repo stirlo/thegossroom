ab1464bbebafa739220e2251cc67aad93f2802b1
stirling campbell <3212918+stirlo@users.noreply.github.com>
2025-08-02 20:31:46 +1000

    Create tag_generator.rb

---
+++ _plugins/tag_generator.rb
@@ -0,0 +1,26 @@
+module Jekyll
+  class TagPageGenerator < Generator
+    safe true
+
+    def generate(site)
+      site.tags.each do |tag, posts|
+        site.pages << TagPage.new(site, site.source, tag, posts)
+      end
+    end
+  end
+
+  class TagPage < Page
+    def initialize(site, base, tag, posts)
+      @site = site
+      @base = base
+      @dir = "tag/#{tag}"
+      @name = 'index.html'
+
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'tag.html')
+      self.data['tag'] = tag
+      self.data['posts'] = posts
+      self.data['title'] = "Posts tagged '#{tag}'"
+    end
+  end
+end
