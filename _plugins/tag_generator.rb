02ae9f6a36aceffbdfb1fdba7fea17e856406e37
stirling campbell <3212918+stirlo@users.noreply.github.com>
2025-08-07 20:05:42 +1000

    Update tag_generator.rb
    hyphen or underscore? doesnt matter

--- _plugins/tag_generator.rb
+++ _plugins/tag_generator.rb
@@ -3,27 +3,48 @@ module Jekyll
     safe true
 
     def generate(site)
+      # First pass - create pages for actual tags
       site.tags.each do |tag, posts|
-        # Skip empty or nil tags
+        next if tag.nil? || tag.empty?
+        site.pages << TagPage.new(site, site.source, tag, posts)
+      end
+
+      # Second pass - create alias pages for underscore/hyphen variants
+      site.tags.each do |tag, posts|
         next if tag.nil? || tag.empty?
 
-        site.pages << TagPage.new(site, site.source, tag, posts)
+        if tag.include?('_')
+          alias_tag = tag.gsub('_', '-')
+          # Only create alias if it doesn't already exist as a real tag
+          unless site.tags.key?(alias_tag)
+            site.pages << TagPage.new(site, site.source, alias_tag, posts, tag)
+          end
+        elsif tag.include?('-') && tag.start_with?('source-')
+          alias_tag = tag.gsub('-', '_')
+          unless site.tags.key?(alias_tag)
+            site.pages << TagPage.new(site, site.source, alias_tag, posts, tag)
+          end
+        end
       end
     end
   end
 
   class TagPage < Page
-    def initialize(site, base, tag, posts)
+    def initialize(site, base, tag, posts, canonical_tag = nil)
       @site = site
       @base = base
-      @dir = "tag/#{Jekyll::Utils.slugify(tag)}"  # This handles special chars
+      @dir = "tag/#{tag}"
       @name = 'index.html'
 
       self.process(@name)
       self.read_yaml(File.join(base, '_layouts'), 'tag.html')
-      self.data['tag'] = tag
+
+      # Use canonical tag for data lookup, display tag for URL
+      display_tag = canonical_tag || tag
+      self.data['tag'] = display_tag
       self.data['posts'] = posts
-      self.data['title'] = "Posts tagged '#{tag}'"
+      self.data['title'] = "Posts tagged '#{display_tag}'"
+      self.data['canonical_tag'] = canonical_tag
     end
   end
 end
